936c6a026d530421b64d0534cb9e6d0bb2745989
IH Sohag <injamulhaqsohag19977@gmail.com>
2022-11-05 02:34:18 +0600

    splash screen design

---
+++ lib/constant.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color deep_orrange = Color(0xFFFF6B6B);
+}
---
+++ lib/views/screens/splash_screen.dart
@@ -0,0 +1,48 @@
+import 'package:e_commerce/constant.dart';
+import 'package:e_commerce/views/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () => Get.to(
+        () => HomeScreen(),
+      ),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.deep_orrange,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "E-Commerce",
+              style: TextStyle(
+                  fontSize: 25.sp,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500),
+            ),
+            SizedBox(height: 20.h),
+            CircularProgressIndicator(color: Colors.white),
+          ],
+        ),
+      ),
+    );
+  }
+}
